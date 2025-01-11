2709ff7ca6b279265e1c2f9aa9026397a32de41c
pooja <pooja@gamil.com>
2025-01-11 10:48:54 +0530

    index change

--- index.ts
+++ index.ts
@@ -1,31 +1,35 @@
-import { Request, Response } from "express";
-// const mongoose = require("mongoose")
-import mongoose from 'mongoose'
-import express from 'express'
-import cors from 'cors'
+// import express, { Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import express, { NextFunction, Request, Response } from 'express'
+import mongoose from "mongoose";
+import todoRoutes from './routes/todo.route'
 
-import todoRoutes from "./routes/todo.route"
-import { Callback } from "mongoose";
-
-require("dotenv").config()
+dotenv.config()
 
 const app = express()
 app.use(express.json())
 app.use(cors())
-// app.use(cors({ origin: true, credentials: true }))
-app.use('/api/v1/todos', todoRoutes);
 
-app.use("*", (req: Request, res: Response) => {
-    res.status(404).json({ message: "Route Not Found" })
-})
-app.use((err: Error, req: Request, res: Response, next: Callback) => {
-    res.status(500).json({ message: "Server Error", error: err?.message })
-})
-mongoose.connect(process.env.MONGO_URL as string)
+const mongoUrl = process.env.MONGO_URL
+if (!mongoUrl) {
+    throw new Error("MONGO_URL is not defined in environment variables.")
+}
 
-mongoose.connection.once("open", () => {
-    console.log("Mongo Connected")
-    app.listen(process.env.PORT, () => {
-        console.log(`Server Running on Port ${process.env.PORT}`)
-    })
+mongoose
+    .connect(mongoUrl)
+    .then(() => console.log("MONGO_CONNECTED"))
+    .catch((err) => console.error("Failed to connect to MongoDB:", err.message))
+
+app.use("/api/todos", todoRoutes)
+app.use((req: Request, res: Response) => {
+    res.status(404).json({ message: "Resource Not Found" });
+})
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.message);
+    res.status(500).json({ message: "Internal Server Error", error: err.message })
+})
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`SERVER RUNNING on port${port}`)
 })
